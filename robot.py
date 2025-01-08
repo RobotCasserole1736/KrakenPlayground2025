17f6bb6dd5445d6c9f458884c6ca57086a0b7496
Chris Gerth <chrisgerth010592@gmail.com>
2025-01-07 20:22:44 -0600

    both impelmentations seem to be working

--- robot.py
+++ robot.py
@@ -1,11 +1,31 @@
+import wpilib
+
+from utils.signalLogging import SignalWrangler
+from wrapperedSparkMax import WrapperedSparkMax 
 from wrapperedKraken import WrapperedKraken
-import wpilib 
 
 class MyRobot(wpilib.TimedRobot):
     def __init__(self):
         wpilib._wpilib.TimedRobot.__init__(self)
-        self.testMotor = WrapperedKraken(3, "testKraken")
+        self.testKraken = WrapperedKraken(3, "testKraken")
+        self.testSparkMax = WrapperedSparkMax(10, "testSparkMax")
+        self.testKraken.setPID(0.3,0.0,0.0)
+        self.testSparkMax.setPID(0.05,0.0,0.0)
+
+        self.tmpCmd = 200.0
+        self.lastTime = wpilib.Timer.getFPGATimestamp()
+
 
     def teleopPeriodic(self):
-        self.testMotor.setVoltage(1.0)
-        wpilib.SmartDashboard.putNumber("Test Kraken Position", self.testMotor.getMotorPositionRad())
+
+        if(wpilib.Timer.getFPGATimestamp() - self.lastTime > 1.0):
+            self.tmpCmd *= -1.0
+            self.lastTime = wpilib.Timer.getFPGATimestamp()
+
+        self.testKraken.setVelCmd(self.tmpCmd/10.0)
+        self.testSparkMax.setPosCmd(self.tmpCmd)
+        
+        wpilib.SmartDashboard.putNumber("Test Kraken Position", self.testKraken.getMotorPositionRad())
+        wpilib.SmartDashboard.putNumber("Test SparkMax Position", self.testSparkMax.getMotorPositionRad())
+
+        SignalWrangler().update()
